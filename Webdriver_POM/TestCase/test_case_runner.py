5c106da375bbea5ff8b571a0f29898a7b256a83e
卡尔西 <HipHopCoderS@users.noreply.github.com>
2025-03-30 13:15:47 +0800

    Update test_case_runner.py

--- Webdriver_POM/TestCase/test_case_runner.py
+++ Webdriver_POM/TestCase/test_case_runner.py
@@ -1,1 +1,16 @@
-# -*- coding:utf-8 -*-import unittestfrom xiaomi_Test import  XiaoMiTestdef suite1():	suite1 = unittest.TestSuite()	suite1.addTest(XiaoMiTest('test_login'))	return suite1if __name__ == '__main__':	runner = unittest.TextTestRunner()	runner.run(suite1())
+# -*- coding:utf-8 -*-
+
+import unittest
+
+from xiaomi_Test import  XiaoMiTest
+
+def suite1():
+	suite1 = unittest.TestSuite()
+	suite1.addTest(XiaoMiTest('test_login'))
+	return suite1
+
+
+
+if __name__ == '__main__':
+	runner = unittest.TextTestRunner()
+	runner.run(suite1())
